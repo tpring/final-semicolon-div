df9e28de73f54de37da9da6ea250e30f7295b59c
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-07-22 12:01:54 +0900

    feature: matching color of h1 and routes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,15 @@
 import Link from 'next/link';
 import SearchButton from '../common/SearchButton';
 import Logo from '../common/Logo';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+
+  const getLinkClasses = (path: string) => {
+    return router.pathname === path ? 'text-purple-500' : 'text-gray-700';
+  };
+
   return (
     <header className="bg-white shadow">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
@@ -14,19 +21,19 @@ const Header = () => {
           </Link>
           <div className="flex space-x-4">
             <Link href={'/'}>
-              <h1>홈</h1>
+              <h1 className={getLinkClasses('/')}>홈</h1>
             </Link>
             <Link href={'/conference'}>
-              <h1>컨퍼런스</h1>
+              <h1 className={getLinkClasses('/conference')}>컨퍼런스</h1>
             </Link>
             <Link href={'/qna'}>
-              <h1>Q&A</h1>
+              <h1 className={getLinkClasses('/qna')}>Q&A</h1>
             </Link>
             <Link href={'/archiving'}>
-              <h1>아카이브</h1>
+              <h1 className={getLinkClasses('/archiving')}>아카이브</h1>
             </Link>
             <Link href={'/notification'}>
-              <h1 className="hidden sm:inline">공지</h1>
+              <h1 className={`hidden sm:inline ${getLinkClasses('/notification')}`}>공지</h1>
             </Link>
           </div>
         </div>
@@ -35,10 +42,10 @@ const Header = () => {
             <SearchButton />
           </Link>
           <Link href={'/signin'}>
-            <h1>로그인</h1>
+            <h1 className={getLinkClasses('/signin')}>로그인</h1>
           </Link>
           <Link href={'/signup'}>
-            <h1>회원가입</h1>
+            <h1 className={getLinkClasses('/signup')}>회원가입</h1>
           </Link>
         </div>
       </div>
