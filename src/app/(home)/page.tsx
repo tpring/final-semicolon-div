baf908964c45a99488d8e3799d66b691950962dc
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-07-23 20:30:19 +0900

    feature:header api

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,4 @@
 import Header from '@/components/header/Header';
-
-const HomePage = () => {
-  return (
 import BestForum from './_components/BestForum';
 import LandingPage from './_components/LandingPage';
 import MainPageTag from './_components/MainPageTag';
@@ -15,7 +12,7 @@ import WebsiteLink from './_components/WebsiteLink';
 const HomePage = () => {
   return (
     <div className="flex flex-col justify-center items-center relative">
-            <Header />
+      <Header />
       <div className="xl:w-[1200px] md:w-[800px] sm:w-96 flex flex-col gap-5">
         <PostsLink />
         <LandingPage />
