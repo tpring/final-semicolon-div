72603c0a8ed41963de6d016dd8bbba3b61866720
tpring <pororo1@naver.com>
2024-07-26 14:28:50 +0900

    feature: porfilecontent api

--- src/app/(home)/(profile)/layout.tsx
+++ src/app/(home)/(profile)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProfileSidebar from './_components/ProfileSidebar';
+import Header from '@/components/header/Header';
 
 type ProfileLayoutProps = {
   children: React.ReactNode;
@@ -7,9 +8,12 @@ type ProfileLayoutProps = {
 
 const ProfileLayout = ({ children }: ProfileLayoutProps) => {
   return (
-    <div className="flex justify-center w-[1200px] h-screen">
-      <ProfileSidebar />
-      <main className="flex-1 p-10">{children}</main>
+    <div className="flex flex-col justify-center items-center relative">
+      <Header />
+      <div className="flex justify-center w-[1200px] h-screen">
+        <ProfileSidebar />
+        <main className="flex-1 p-10">{children}</main>
+      </div>
     </div>
   );
 };
