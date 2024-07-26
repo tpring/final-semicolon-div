73784731e7fdd249e5712d902feff01d814df324
Floweringworld <jns8665@naver.com>
2024-07-26 12:13:58 +0900

    refactor: bestForum sort 추가

--- src/types/main-page/bestForum.ts
+++
@@ -1,9 +0,0 @@
-import { Tables } from '../supabase';
-
-export type ResumesType = {
-  users: Tables<'users'>;
-  like: Tables<'forum_likes'>;
-  images: Tables<'forum_images'>;
-  tags: Tables<'forum_tags'>;
-  comments: Tables<'forum_comments'>;
-};
