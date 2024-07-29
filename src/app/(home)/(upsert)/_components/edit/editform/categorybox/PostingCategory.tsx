465e6e09b20f186bc43d89ae26978879f34feef4
YCDM03 <ycdm03@gmail.com>
2024-07-29 21:18:24 +0900

    delete: unused component

--- src/app/(home)/(upsert)/_components/edit/editform/categorybox/PostingCategory.tsx
+++ src/app/(home)/(upsert)/_components/edit/editform/categorybox/PostingCategory.tsx
@@ -2,7 +2,7 @@ import { usePostingCategoryStore } from '@/store/postingCategoryStore';
 import UpsertTheme from '../../../UpsertTheme';
 
 const PostingCategory = () => {
-  const { categoryGroup, subCategory, setCategoryGroup, setSubCategory } = usePostingCategoryStore();
+  const { categoryGroup } = usePostingCategoryStore();
   return (
     <div className="flex flex-col gap-4 my-2">
       <h1 className="font-bold text-xl w-40  text-gray-700  h-[26px]">{categoryGroup.category}</h1>
