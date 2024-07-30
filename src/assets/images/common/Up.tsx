f3639db5259c3044623948b5501c5fa0ad28a117
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-07-30 21:13:42 +0900

    feature: add assets

---
+++ src/assets/images/common/Up.tsx
@@ -0,0 +1,14 @@
+const Up = () => {
+  return (
+    <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 7L6.85858 1.14142C6.93668 1.06332 7.06332 1.06332 7.14142 1.14142L13 7"
+        stroke="#0F0F0F"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default Up;
