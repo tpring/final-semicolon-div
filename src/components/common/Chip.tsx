721fc96aeb180c211b2d4881242c108bbf4306e5
YCDM03 <ycdm03@gmail.com>
2024-08-06 21:45:41 +0900

    update: reusable button ui

---
+++ src/components/common/Chip.tsx
@@ -0,0 +1,64 @@
+import { cva } from 'class-variance-authority';
+import '@/app/globals.css';
+import { ComponentProps } from 'react';
+
+const largeChipVarients = cva(['text-subtitle1', 'font-bold', 'content-center', 'text-center'], {
+  variants: {
+    intent: {
+      primary: ['bg-main-400', 'text-white', 'hover:bg-main-500', 'active:bg-main-500'],
+      primary_disabled: ['bg-main-100', 'text-neutral-50'],
+      secondary: ['bg-main-50', 'text-main-400', 'hover:bg-main-100', 'active:bg-main-100'],
+      secondary_disabled: ['bg-main-50', 'text-main-100'],
+      gray: ['bg-neutral-50', 'text-neutral-500', 'hover:bg-neutral-100', 'active:text-neutral-500'],
+      gray_disabled: ['bg-neutral-50', 'text-neutral-100'],
+      default: [
+        'bg-white',
+        'border border-neutral-200',
+        'text-neutral-500',
+        'hover:bg-neutral-50',
+        'active:bg-neutral-50'
+      ],
+      default_disabled: ['bg-white', 'border border-neutral-200', 'text-neutral-100']
+    },
+    size: {
+      large: 'px-6 py-4 w-[130px] h-14 rounded-lg',
+      medium: 'px-5 py-3 w-[122px] h-12 rounded-md',
+      small: 'px-4 py-2 w-[114px] h-10 rounded'
+    }
+  },
+
+  compoundVariants: [
+    {
+      intent: 'primary',
+      size: 'medium'
+    }
+  ],
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium'
+  }
+});
+
+type ChipProps = ComponentProps<'button'> & {
+  label: string;
+  intent?:
+    | 'primary'
+    | 'primary_disabled'
+    | 'secondary'
+    | 'secondary_disabled'
+    | 'gray'
+    | 'gray_disabled'
+    | 'default'
+    | 'default_disabled';
+  size?: 'large' | 'medium' | 'small';
+};
+
+function Chip({ label, intent, size, ...props }: ChipProps) {
+  return (
+    <button className={largeChipVarients({ intent, size })} {...props}>
+      {label}
+    </button>
+  );
+}
+
+export default Chip;
